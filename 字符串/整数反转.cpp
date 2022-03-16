d84f408e344586c2181dd5a4c7cebcae03adcacb
drowning-in-codes <bukalala174@gmail.com>
2022-03-16 17:41:57 +0800

    update

--- 字符串/整数反转.cpp
+++ 字符串/整数反转.cpp
@@ -3,36 +3,30 @@
  * @Date: 2022-03-15 11:15:20
  * @LastEditTime: 2022-03-15 11:15:20
  * @LastEditors: proanimer
- * @Description: 
+ * @Description:
  */
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    class Solution {
-    public:
-    int reverse(int x) {
-         int res = 0;
-        while (x != 0) {
-            int t = x % 10;
-            int newRes = res * 10 + t;
-            //如果数字溢出，直接返回0
-            cout<<newRes<<" "<<res<<endl;
-            if ((newRes - t) / 10 != res)
-                return 0;
-            res = newRes;
-            x = x / 10;
-        }
-        return res;
-    }
-    Solution()
+    class Solution
     {
+    public:
+        int reverse(int x)
+        {
+            int res = 0;
+            while (x != 0)
+            {
+                int t = x % 10;
+                //如果数字溢出，直接返回0
+                if (abs(res) > INT_MAX / 10)
+                    return 0;
+                int newRes = res * 10 + t;
 
-    }
-};
-
-    Solution mysolution =  Solution();
-   int r =  mysolution.reverse(1534236469);
-    cout<<r;
+                res = newRes;
+                x = x / 10;
+            }
+            return res;
+        }
+    };
 }
-
